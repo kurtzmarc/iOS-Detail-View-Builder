86cce3641d7d8ad94310bf663f2253e4b02ab216
Marc Kurtz <kurtzmarc@yahoo.com>
2011-12-29 17:37:42 -0500

    Initial check in of library, tests, and demo.

---
+++ iOSDetailViewBuilderDemo/iOSDetailViewBuilderDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSDetailViewBuilderDemo
+//
+//  Created by Marc Kurtz on 12/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
